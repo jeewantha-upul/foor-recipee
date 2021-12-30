f53a28b7bf346d026bd93e8576bb12ec1758b032
jeewantha upul <59133288+jeewantha-upul@users.noreply.github.com>
2021-12-30 23:22:39 +0530

    searching food by name finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./key";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
-  let url =
-    "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+  const [query, setQuery] = useState("");
 
+  // API ID and Key
+  const YOUR_APP_ID = "8af20a67";
+  const YOUR_APP_KEY = "1c1ac44957672692026e42e886860181";
+
+  // API endpoint
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  //API call
+  const getData = async () => {
+    let recipies = await Axios.get(url);
+    console.log(recipies.data);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    getData();
+  };
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="app">
+      <h1>Food Recipee </h1>
+
+      <form action="" className="app__searchForm" onSubmit={submitHandler}>
+        <input
+          className="app__input"
+          type="text"
+          value={query}
+          placeholder="Enter engredient"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input className="app_submit" type="submit" value="search" />
+      </form>
     </div>
   );
 }
