1cb4f3adb79e28eea9fcc6aa7616e17b3198f2a1
jeewantha upul <59133288+jeewantha-upul@users.noreply.github.com>
2021-12-31 09:58:29 +0530

    searching functionality based on health labels

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import RecipeTile from "./RecipeTile";
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [healthLabel, setHealthLabel] = useState("vegan");
 
   // API ID and Key
   const YOUR_APP_ID = "8af20a67";
   const YOUR_APP_KEY = "1c1ac44957672692026e42e886860181";
 
   // API endpoint
-  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabel}`;
   //API call
   const getData = async () => {
     let result = await Axios.get(url);
@@ -29,6 +30,7 @@ function App() {
     <div className="app">
       <h1>Food Recipee </h1>
 
+      {/* searching recipes */}
       <form action="" className="app__searchForm" onSubmit={submitHandler}>
         <input
           className="app__input"
@@ -40,6 +42,22 @@ function App() {
         <input className="app_submit" type="submit" value="search" />
       </form>
 
+      <select className="app__healthLabels">
+        <option onClick={() => setHealthLabel("vegan")}>Vegan</option>
+        <option onClick={() => setHealthLabel("vegetarian")}>vegetarian</option>
+        <option onClick={() => setHealthLabel("paleo")}>paleo</option>
+        <option onClick={() => setHealthLabel("dairy-free")}>dairy-free</option>
+        <option onClick={() => setHealthLabel("gluten-free")}>
+          gluten-free
+        </option>
+        <option onClick={() => setHealthLabel("wheat-free")}>wheat-free</option>
+        <option onClick={() => setHealthLabel("egg-free")}>egg-free</option>
+        <option onClick={() => setHealthLabel("peanut-free")}>
+          peanut-free
+        </option>
+      </select>
+
+      {/* rendering recipes */}
       <div className="app__recipes">
         {recipes.map((recipe) => (
           <RecipeTile recipe={recipe} />
